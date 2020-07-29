c122a6012fcbe88f10b702ef9e5f51a95b3abe73
DeboraCristinaGoncalves <debora.cristina@motoboy.com>
2020-07-29 13:00:24 -0300

    Exercícios Vetores - Java

---
+++ Java/Curso/Vetores/Exercicio27.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+public class Exercicio27 {
+
+	public static void main(String[] args) {
+		// 27
+
+		Random aleatorio = new Random();
+
+		ArrayList<Integer> vetor = new ArrayList<Integer>();
+		Set<Integer> numerosNaoPrimos = new HashSet<Integer>();
+		Set<Integer> numerosPrimos = new HashSet<Integer>();
+		
+		for (int i = 0; i < 10; i++) {
+			vetor.add(aleatorio.nextInt(11) + 0);
+		}
+
+		numerosPrimos.addAll(vetor);
+		
+		System.out.println("Vetor: " + vetor);
+		System.out.println("Números primos: " + numerosPrimos);
+
+		/* Números não primos */
+
+		for (int valor : vetor) {	
+			for (int j = valor; j > 0; j--) {
+				if (j != 1 && j != valor) {
+					if (valor % j == 0) {
+						numerosNaoPrimos.add(valor);
+						numerosPrimos.remove(valor);
+					}
+				}
+
+				}
+
+		}
+				
+		System.out.println("Números não primos: " + numerosNaoPrimos);
+		System.out.println("Números primos: " + numerosPrimos);
+
+		} 
+		
+		/* Remover elementos repetidos ArrayList e criar numerosPrimos */
+	}
+
