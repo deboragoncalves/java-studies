b25e5aa6c284c121609470fd0a6cf7baa1ddd53b
DeboraCristinaGoncalves <debora.cristina@motoboy.com>
2020-07-31 16:45:43 -0300

    Exercícios Matrizes - Java

--- Java/Curso/Matrizes/Exercicio14.java
+++ Java/Curso/Matrizes/Exercicio14.java
@@ -5,37 +5,53 @@ import java.util.Set;
 public class Exercicio14 {
 
 	public static void main(String[] args) {
-		// 14 (incompleto)
-		
-		Random aleatorio = new Random();
-	    Set<Integer> listaNumero = new HashSet<>();
-	    int numero = 0;
-	    
-	    int matriz[][] = new int[5][5];
-		
-	    for (int i = 0; i < matriz.length; i++) {
-	    	numero = aleatorio.nextInt((99 - 0 + 1) + 0);
-	    	System.out.println("Número: " + numero);
-	    	listaNumero.add(numero);
-	    }
-	    
-	    for (int valor : listaNumero) {
-	    	for (int i = 0; i < matriz.length; i++) {
-	    		for (int j = 0; j < matriz[i].length; j++) {
-	    			matriz[i][j] = valor;
-	    		}
-	    	}
-	    }
-	    
-	    System.out.println("Matriz: ");
-	    System.out.println();
-		for (int i = 0; i < matriz.length; i++) {
-			for (int j = 0; j < matriz[i].length; j++) {
-				/* Imprime todos os elementos de uma coluna */
-				System.out.print(matriz[i][j] + " ");
-			}
-			System.out.println();
-		}
-	}
+		// 14 
 
+		Random aleatorio = new Random();
+
+		Set<Integer> numerosAleatorios = new HashSet<Integer>();
+		int matriz[][] = new int[5][5];
+		int numero = 0;
+		int novoNumero = 0;
+		int contador1 = 0;
+		int contador2 = 0;
+		int elementosMatriz[] = new int[25];
+		
+		
+			for (int i = 0; i < matriz.length; i++) {
+				for (int j = 0; j < matriz[i].length; j++) {
+					numero = aleatorio.nextInt((99 - 0 + 1) + 0);
+					numerosAleatorios.add(numero);
+				}
+			}
+			
+			while (numerosAleatorios.size() < 25) {
+				novoNumero = aleatorio.nextInt((99 - 0 + 1) + 0);
+				numerosAleatorios.add(novoNumero);
+			}
+		
+			for (int valor : numerosAleatorios) {
+				elementosMatriz[contador1] = valor;
+				contador1++;
+			}
+			
+			for (int i = 0; i < matriz.length; i++) {
+				/* Imprimir todos os elementos de uma linha */
+				for (int j = 0; j < matriz[i].length; j++) {
+					matriz[i][j] = elementosMatriz[contador2];
+					contador2++;
+				}
+			}
+			
+			System.out.println();
+			System.out.println("Cartela de bingo com números diferentes: ");
+			for (int i = 0; i < matriz.length; i++) {
+				for (int j = 0; j < matriz[i].length; j++) {
+					System.out.print(matriz[i][j] + " ");
+				}
+				System.out.println();
+			}
+
+
+	}
 }
