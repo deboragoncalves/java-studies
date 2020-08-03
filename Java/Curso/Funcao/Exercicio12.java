0de4a670a4b44094fdff2a84f3f053695fc7cab0
DeboraCristinaGoncalves <debora.cristina@motoboy.com>
2020-08-03 16:18:18 -0300

    Exercicios Matrizes

---
+++ Java/Curso/Funcao/Exercicio12.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+
+public class Exercicio12 {
+
+	public static void main(String[] args) {
+		// 12
+		
+		Random aleatorio = new Random();
+
+		somaAlgarismos(aleatorio.nextInt(200) + 1);
+
+	}
+	
+	public static void somaAlgarismos(int numero) {
+		if (numero > 0) {
+			
+			char digitos[] = String.valueOf(numero).toCharArray();
+			
+			for (char a : digitos) {
+				System.out.print(a + " ");
+			}
+		} else {
+			System.out.println("O número é inválido");
+		}
+		
+	}
+
+}
