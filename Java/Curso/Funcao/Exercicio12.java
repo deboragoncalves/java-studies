0b96ca6b00f6de30d8c7c8c9f93ecca836085c30
Débora <debora.cristina@motoboy.com>
2020-08-04 00:05:19 -0300

    Exercicio 12 - Função - Java

--- Java/Curso/Funcao/Exercicio12.java
+++ Java/Curso/Funcao/Exercicio12.java
@@ -1,28 +1,32 @@
+package funcao;
+
 import java.util.Random;
 
 public class Exercicio12 {
 
 	public static void main(String[] args) {
 		// 12
-		
+
 		Random aleatorio = new Random();
 
 		somaAlgarismos(aleatorio.nextInt(200) + 1);
 
 	}
-	
+
 	public static void somaAlgarismos(int numero) {
 		if (numero > 0) {
+			System.out.println("Número: " + numero);
 			
 			char digitos[] = String.valueOf(numero).toCharArray();
-			
+
+			System.out.print("Algarismos: ");
 			for (char a : digitos) {
 				System.out.print(a + " ");
 			}
+			
 		} else {
 			System.out.println("O número é inválido");
 		}
-		
-	}
 
+	}
 }
