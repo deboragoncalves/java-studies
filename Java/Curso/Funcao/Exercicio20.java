604115d5ed54b41485cb3673837849a1e24a4247
Débora <debora.cristina@motoboy.com>
2020-08-04 15:51:35 -0300

    Exercicios Funcoes

---
+++ Java/Curso/Funcao/Exercicio20.java
@@ -0,0 +1,27 @@
+package funcao;
+
+import java.util.Random;
+
+public class Exercicio20 {
+
+	public static void main(String[] args) {
+		// 20
+		
+		Random aleatorio = new Random();
+		
+		System.out.println(fatorial(aleatorio.nextInt(20) + 0));
+	}
+	
+	public static int fatorial(int numero) {
+		int resultado = numero;
+		System.out.println("Número: " + numero);
+		
+		for (int i = numero - 1; i > 0; i--) {
+			resultado *= i;
+		}
+		
+		System.out.print("Resultado: ");
+		return resultado;
+	}
+
+}
