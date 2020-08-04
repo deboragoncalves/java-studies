3ee33a8006804a13505a073bc20bbefb103a11d6
Débora <debora.cristina@motoboy.com>
2020-08-04 00:31:09 -0300

    Exercicio 12 - Funcao - Java

--- Java/Curso/Funcao/Exercicio12.java
+++ Java/Curso/Funcao/Exercicio12.java
@@ -9,24 +9,45 @@ public class Exercicio12 {
 
 		Random aleatorio = new Random();
 
-		somaAlgarismos(aleatorio.nextInt(200) + 1);
+		System.out.println(somaAlgarismos(aleatorio.nextInt(200) + 1));
 
 	}
 
-	public static void somaAlgarismos(int numero) {
+	public static int somaAlgarismos(int numero) {
 		if (numero > 0) {
 			System.out.println("Número: " + numero);
 			
-			char digitos[] = String.valueOf(numero).toCharArray();
-
-			System.out.print("Algarismos: ");
-			for (char a : digitos) {
-				System.out.print(a + " ");
+			String digitos = String.valueOf(numero);
+			
+			int somatorio = 0;
+			
+			System.out.print("Soma dos algarismos: ");
+			if (numero < 100) {
+				String digito1 = digitos.substring(0, 1);
+				String digito2 = digitos.substring(1, 2);
+				
+				int numero1 = Integer.parseInt(digito1);
+				int numero2 = Integer.parseInt(digito2);
+				
+				somatorio = numero1 + numero2;
+				return somatorio;
+			} else {
+				String digito1 = digitos.substring(0, 1);
+				String digito2 = digitos.substring(1, 2);
+				String digito3 = digitos.substring(2, 3);
+				
+				int numero1 = Integer.parseInt(digito1);
+				int numero2 = Integer.parseInt(digito2);
+				int numero3 = Integer.parseInt(digito3);
+				
+				somatorio = numero1 + numero2 + numero3;
+				return somatorio;
 			}
 			
 		} else {
 			System.out.println("O número é inválido");
 		}
+		return 0;
 
 	}
 }
