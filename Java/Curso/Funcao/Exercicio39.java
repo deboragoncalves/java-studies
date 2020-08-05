d3ce9574e6e3936bf2d14d456d02b9ec50608fd6
Débora <debora.cristina@motoboy.com>
2020-08-05 19:13:15 -0300

    Exercícios Funções - Java

---
+++ Java/Curso/Funcao/Exercicio39.java
@@ -0,0 +1,32 @@
+package funcao;
+
+import java.util.Random;
+
+public class Exercicio39 {
+
+	public static void main(String[] args) {
+		// 39
+		
+		Random aleatorio = new Random();
+		
+		troca(aleatorio.nextInt(15) + 1, aleatorio.nextInt(15) + 1);
+
+	}
+	
+	public static void troca(int numero1, int numero2) {
+		System.out.println("Original: ");
+		System.out.println("Número 1: " + numero1);
+		System.out.println("Número 2: " + numero2);
+
+		int numeroA = numero1;
+		int numeroB = numero2;
+		numero2 = numeroA;
+		numero1 = numeroB;
+		
+		System.out.println();
+		System.out.println("Troca: ");
+		System.out.println("Número 1: " + numero1);
+		System.out.println("Número 2: " + numero2);
+	}
+
+}
