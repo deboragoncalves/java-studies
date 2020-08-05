dab298c568a6f80168b3d9921e8cdf498045ba7c
Débora <debora.cristina@motoboy.com>
2020-08-05 13:42:05 -0300

    Exercício 27 - Funcao - Java

--- Java/Curso/Funcao/Exercicio27.java
+++ Java/Curso/Funcao/Exercicio27.java
@@ -1,5 +1,6 @@
 package funcao;
 
+import java.text.DecimalFormat;
 import java.util.Random;
 
 public class Exercicio27 {
@@ -8,14 +9,39 @@ public class Exercicio27 {
 		// 27
 
 		Random aleatorio = new Random();
-		
+
 		System.out.println(senoTaylor(aleatorio.nextInt(100) + 1));
 	}
-	
-	public static int senoTaylor(int angulo) {
+
+	public static String senoTaylor(int angulo) {
+		DecimalFormat formatacaoDecimal = new DecimalFormat("0.00");
+
 		System.out.println("Ângulo em graus: " + angulo);
+
 		double pi = 3.141593;
 		double radianos = (double) (angulo * pi) / 180;
+		System.out.println("Ângulo em radianos: " + formatacaoDecimal.format(radianos));
+
+		double somatorio = 0.0;
+		int fatorial = 1;
+
+		for (int i = 0; i <= 5; i++) {
+			int numero = 2 * i + 1;
+			int numeroFatorial = numero;
+			
+			while (numeroFatorial > 0) {
+				fatorial *= numeroFatorial;
+				numeroFatorial--;
+			}
+			
+			double divisao = (double) (Math.pow(radianos, numero) / fatorial);
+			somatorio += divisao;
+			fatorial = 1;
+		}
+		
+		System.out.print("Série de Taylor: ");
+		return formatacaoDecimal.format(somatorio);
+	
 	}
 
 }
