75b0bf5593c4bf15cc9c73baa93a3c44a1a61a6b
Débora <debora.cristina@motoboy.com>
2020-08-05 17:57:17 -0300

    Exercícios Função - Java

---
+++ Java/Curso/Funcao/Exercicio34.java
@@ -0,0 +1,30 @@
+package funcao;
+
+import java.util.Random;
+
+public class Exercicio34 {
+
+	public static void main(String[] args) {
+		// 34
+
+		Random aleatorio = new Random();
+
+		System.out.println(duploFatorial(aleatorio.nextInt(15) + 1));
+	}
+
+	public static int duploFatorial(int numero) {
+		System.out.println("Número: " + numero);
+		int fatorial = 1;
+
+		for (int i = numero; i > 0; i--) {
+			if (i % 2 != 0) {
+				fatorial *= i;
+			}
+		}
+
+		System.out.print("Duplo fatorial de " + numero + ": ");
+		return fatorial;
+	
+	}
+
+}
---
+++ Java/Curso/Funcao/Exercicio35.java
@@ -0,0 +1,39 @@
+package funcao;
+
+import java.text.DecimalFormat;
+import java.util.Random;
+
+public class Exercicio35 {
+
+	public static void main(String[] args) {
+		// 35
+		
+		Random aleatorio = new Random();
+		
+		System.out.println(fatorialQuadruplo(aleatorio.nextInt(15) + 1));
+	}
+	
+	public static String fatorialQuadruplo(int numero) {
+		System.out.println("Número: " + numero);
+		
+		DecimalFormat formatacaoDecimal = new DecimalFormat("0.00");
+
+		int numerador = 2 * numero;
+		int fatorialDenominador = 1;
+		int fatorialNumerador = 1;
+		
+		for (int i = numerador; i > 0; i--) {
+			fatorialNumerador *= i;
+		}
+		
+		for (int i = numero; i > 0; i--) {
+			fatorialDenominador *= i;
+		}
+		
+		double divisao = (double) fatorialNumerador / fatorialDenominador;
+		
+		System.out.print("Fatorial Quádruplo de " + numero + ": ");
+		return formatacaoDecimal.format(divisao);
+	}
+
+}
