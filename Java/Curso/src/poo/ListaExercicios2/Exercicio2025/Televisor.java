d3d953ef689a28cd001872e2ee60fa77791c9dc6
Débora <debora.cristina@motoboy.com>
2020-08-17 17:47:12 -0300

    Exercícios POO - Java

---
+++ Java/Curso/src/poo/ListaExercicios2/Exercicio2025/Televisor.java
@@ -0,0 +1,73 @@
+package heranca.Exercicio2025;
+
+public class Televisor {
+	// 20 a 25
+
+	private boolean ligado = false;
+	private int canal = 0;
+	private int volume = 0;
+	private int numeroCanais = 30;
+	private int volumeMaximo = 30;
+	
+	public Televisor() {
+		
+	}
+	
+	public Televisor(boolean estadoAtual, int canalAtual, int volumeAtual, int volumeMaximo, int numeroCanais) {
+		this.ligado = estadoAtual;
+		this.canal = canalAtual;
+		this.volume = volumeAtual;
+		this.numeroCanais = numeroCanais;
+		this.volumeMaximo = volumeMaximo;
+	}
+	
+	public void ligar() {
+		this.ligado = true;
+	}
+	
+	public void desligar() {
+		this.ligado = false;
+	}
+	
+	public void canalAcima() {
+		if (this.canal < this.numeroCanais) {
+			this.canal += 1;
+		} else {
+			this.canal = 1;
+		}
+	}
+	
+	public void canalAbaixo() {
+		if (this.canal < this.numeroCanais) {
+			if (this.canal <= 1) {
+				this.canal = this.numeroCanais;
+			} else {
+				this.canal -= 1;
+			}
+		}
+	}
+	
+	public void volumeAcima() {
+		if (this.volume < this.volumeMaximo) {
+			if (this.volume == this.volumeMaximo) {
+				return;
+			}
+			
+			this.volume += 1;
+		} 
+	}
+	
+	public void volumeAbaixo() {
+		if (this.volume < this.volumeMaximo) {
+			if (this.volume <= 0) {
+				return;
+			}
+			this.volume -= 1;
+		} 
+	}
+	
+	public String toString() {
+		return "Ligado: " + this.ligado + "\nCanal: " + this.canal + "\nVolume: " + this.volume
+				+ "\nVolume máximo: " + this.volumeMaximo + "\nNúmero máximo de canais: " + this.numeroCanais;
+	}
+}
