8900824495535a0d980981a763fd5cd5560379c1
Debora Goncalves <Debora.Goncalves@senior.com.br>
2020-12-01 12:09:04 -0300

    Desafio 05 - parte 1 - oo

---
+++ Java/Curso02-Java/src/com/debora/exercicios/orientacaoobjetos/parte01/desafio05/Dinner.java
@@ -0,0 +1,24 @@
+package com.debora.exercicios.orientacaoobjetos.parte01.desafio05;
+
+public class Dinner {
+
+    /*
+
+    Desafio 06:
+    Classe Pessoa: atributos nome, peso, método comer e toString()
+    Classe Comida: atributos nome, peso.
+    Classe Jantar: método main.
+
+     */
+
+    public static void main(String[] args) {
+
+        Person person01 = new Person("João", 59.90);
+        Food food01 = new Food("Bolo", 0.9);
+
+        person01.eat(food01);
+
+        System.out.println(person01);
+
+    }
+}
