647027b20e8fd166d1302a401ae0be9e053d5f4a
Debora Goncalves <Debora.Goncalves@senior.com.br>
2020-12-01 11:36:22 -0300

    Desafio 03 - parte 01 - oo

---
+++ Java/Curso02-Java/src/com/debora/exercicios/orientacaoobjetos/parte01/desafio03/Company.java
@@ -0,0 +1,15 @@
+package com.debora.exercicios.orientacaoobjetos.parte01.desafio03;
+
+public class Company {
+
+    /* Desafio 3: Utilizar variável estática e não constante. */
+
+    public static void main(String[] args) {
+
+        Ticket ticket1 = new Ticket("João", 50.0);
+        Ticket.discount = 0.25;
+
+        System.out.println(ticket1);
+        
+    }
+}
